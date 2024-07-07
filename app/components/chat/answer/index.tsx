cc85fa37f08c892ee9da5cdda84554a6f67b593a
Dell Di <875372314@qq.com>
2024-07-07 15:35:38 +0800

    🎨 嵌入集成样式优化完毕

--- app/components/chat/answer/index.tsx
+++ app/components/chat/answer/index.tsx
@@ -175,12 +175,12 @@ const Answer: FC<IAnswerProps> = ({
             </div>
           }
         </div>
-        <div className={`${s.answerWrap}`}>
-          <div className={`${s.answer} relative text-sm text-gray-900`}>
+        <div className={`${s.answerWrap}`} style={{ width: 'calc(100% - 2.5rem)' }}>
+          <div className={`${s.answer} relative w-full text-sm text-gray-900`}>
             <div className={`ml-2 py-3 px-4 bg-gray-100 rounded-tr-2xl rounded-b-2xl ${workflowProcess && 'min-w-[480px]'}`}>
-              {workflowProcess && (
+              {/* {workflowProcess && (
                 <WorkflowProcess data={workflowProcess} hideInfo />
-              )}
+              )} */}
               {(isResponsing && (isAgentMode ? (!content && (agent_thoughts || []).filter(item => !!item.thought || !!item.tool).length === 0) : !content))
                 ? (
                   <div className='flex items-center justify-center w-6 h-5'>
